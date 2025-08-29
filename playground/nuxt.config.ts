6181ec2db1fb59cc70edd7a1720236a741030062
Max Engisch <maxengisch@gmail.com>
2025-08-29 07:48:43 +0200

    feat(playground): add flag to switch between versions in the playground

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import path from 'node:path'
 
+const version: 'local' | 'dist' | 'npm' = 'dist'
+const vue_leaflet_plugins = {
+    local: path.resolve(__dirname, '../src/plugins'),
+    dist: '@dist/vue-leaflet-plugins',
+    npm: '@maxel01/vue-leaflet-plugins'
+}
+
 export default defineNuxtConfig({
     ssr: true,
     compatibilityDate: '2025-07-15',
@@ -8,15 +15,13 @@ export default defineNuxtConfig({
     css: ['leaflet/dist/leaflet.css'],
     app: {
         head: {
-            link: [
-                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-            ]
+            link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
         }
     },
     vite: {
         resolve: {
             alias: {
-                '@maxel01/vue-leaflet-plugins': path.resolve(__dirname, '../src/plugins'),
+                '@maxel01/vue-leaflet-plugins': vue_leaflet_plugins[version]
             }
         }
     }
