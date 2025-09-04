966db076ffd9b89f07595c57a7731f9dee1b1aa2
Max Engisch <maxengisch@gmail.com>
2025-09-04 17:48:03 +0200

    feat(leaflet.donut): add playground pages

---
+++ playground/app/pages/leaflet.donut/donut.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { LMap, LTileLayer } from '@maxel01/vue-leaflet'
+import { LDonut } from '@maxel01/vue-leaflet-plugins'
+</script>
+
+<template>
+    <LMap :zoom="12" :center="[47.41322, -1.219482]">
+        <LTileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            layer-type="base"
+            name="OpenStreetMap"
+        />
+        <LDonut :lat-lng="[47.41322, -1.219482]" :radius="2000" :inner-radius="500" />
+    </LMap>
+</template>
---
+++ playground/app/pages/leaflet.donut/index.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { LMap, LTileLayer } from '@maxel01/vue-leaflet'
+import { LDonut } from '@maxel01/vue-leaflet-plugins'
+import { ref } from 'vue'
+
+const radius = ref(2000)
+const innerRadius = ref(500)
+const innerRadiusPercent = ref(0.5)
+const innerRadiusAsPercent = ref(true)
+</script>
+
+<template>
+    <LMap :zoom="12" :center="[47.41322, -1.219482]">
+        <LTileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            layer-type="base"
+            name="OpenStreetMap"
+        />
+        <LDonut
+            :lat-lng="[47.41322, -1.219482]"
+            :radius="radius"
+            :inner-radius="innerRadiusAsPercent ? innerRadiusPercent : innerRadius"
+            :inner-radius-as-percent="innerRadiusAsPercent"
+        />
+    </LMap>
+    <aside>
+        <label>
+            <span>Radius - </span>
+            500 <input v-model.number="radius" type="range" min="500" max="3500" /> 3500
+        </label>
+        <br />
+        <label>
+            <span>Inner Radius - </span>
+            0 <input v-model.number="innerRadius" type="range" min="0" max="1000" /> 1000
+        </label>
+        <br />
+        <label>
+            <span>Inner Radius as Percent - </span>
+            <input v-model.boolean="innerRadiusAsPercent" type="checkbox" />
+        </label>
+        <br />
+        <label>
+            <span>Inner Radius in % - </span>
+            0
+            <input v-model.number="innerRadiusPercent" type="range" min="0" max="1" step="0.02" /> 1
+        </label>
+    </aside>
+</template>
+
+<style>
+aside {
+    max-width: 600px;
+    text-align: center;
+    margin: auto;
+}
+
+label {
+    display: inline-block;
+    padding: 0.5em;
+}
+
+input {
+    vertical-align: text-bottom;
+}
+</style>
